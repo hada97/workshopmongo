f92de752f8e3ef89b4c344b202d038623e344d1f
Adailson <adailson65@gmail.com>
2024-09-16 00:30:47 -0300

    add swagger

---
+++ src/main/java/br/com/workshop/workshopmongo/infra/SpringDocConfigurations.java
@@ -0,0 +1,21 @@
+package br.com.workshop.workshopmongo.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API")
+                        .version("v1")
+                        .description("[GitHub](https://github.com/hada97/workshopmongo)\n\n"
+                        ));
+    }
+}
