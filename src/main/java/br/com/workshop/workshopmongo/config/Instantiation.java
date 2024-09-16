82030bf5f8341564b269a64b10a678592b0ed8ed
Adailson <adailson65@gmail.com>
2024-09-16 11:56:54 -0300

    add comment for posts

--- src/main/java/br/com/workshop/workshopmongo/config/Instantiation.java
+++ src/main/java/br/com/workshop/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package br.com.workshop.workshopmongo.config;
 import br.com.workshop.workshopmongo.domain.Post;
 import br.com.workshop.workshopmongo.domain.User;
 import br.com.workshop.workshopmongo.dto.AuthorDTO;
+import br.com.workshop.workshopmongo.dto.CommentDTO;
 import br.com.workshop.workshopmongo.repository.PostRepository;
 import br.com.workshop.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +38,10 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar pra Manaus!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Dia top", "acordei feliz!", new AuthorDTO(maria));
+
+        CommentDTO c1= new CommentDTO("Boa viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        post1.getComments().addAll(Arrays.asList(c1));
+
         postRepository.saveAll(Arrays.asList(post1,post2));
 
         maria.getPosts().addAll(Arrays.asList(post1,post2));
--- src/main/java/br/com/workshop/workshopmongo/dto/CommentDTO.java
+++ src/main/java/br/com/workshop/workshopmongo/dto/CommentDTO.java
@@ -4,9 +4,24 @@ package br.com.workshop.workshopmongo.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
 @Setter
 @Getter
-public class CommentDTO {
+public class CommentDTO implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 1L;
 
+    private String text;
+    private Date date;
+    private String AuthorDTO;
+
+    public CommentDTO(String text, Date date, AuthorDTO authorDto) {
+        this.text = text;
+        this.date = date;
+        AuthorDTO = AuthorDTO;
+    }
 }
